db6ec724e1a561b4cdb665755dd6fe6fba445fde
Onkar Hule <109545876+ONKARHule@users.noreply.github.com>
2025-03-06 00:54:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.use("/public", express.static(__dirname + "/public"))
+// Serve static files from the 'public' directory
+app.use("/public", express.static(path.join(__dirname, "public")));
 
+// Serve index.html for the root route
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+// 🔹 Fix: Catch-all route to serve index.html for React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start server
 app.listen(1337, () => {
   console.log("The server is up and running!");
 });
