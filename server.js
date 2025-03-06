93a179f2191a9feb81a8bb31744597897f0b8417
Onkar Hule <109545876+ONKARHule@users.noreply.github.com>
2025-03-06 10:56:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(__dirname + "/public"))
 
-// Catch-all route to serve React frontend
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(1337, () => {
+  console.log("The server is up and running!");
 });
